80f0efed0de31c0a047406b1a650d00862c414e7
locious7 <locious7@protonmail.com>
2025-01-26 16:16:47 -0700

    add move functionality, add pygame object groups

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -18,9 +22,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for update in updatable:
+            update.update(dt)
+
         screen.fill(SCREEN_COLOR) 
-        player.draw(screen)
+
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 fps
